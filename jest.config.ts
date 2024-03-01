c77d477ba45aea7554acea51bfdd9400be141bf0
Keloran <keloran@develbox.info>
2024-03-01 13:58:02 +0000

    hmm

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest/presets/js-with-babel',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  clearMocks: true,
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^./App.css': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+};
+
+export default config;
